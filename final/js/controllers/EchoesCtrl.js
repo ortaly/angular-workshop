45f0326c08e43361e1cbdbee16f6694cd6857516
orizens <farhioren@gmail.com>
2013-01-28 21:32:53 +0200

    added a working controller of youtube results
    EchoesCtrl which connects to youtube REST api by the search input

--- final/js/controllers/EchoesCtrl.js
+++ final/js/controllers/EchoesCtrl.js
@@ -1,3 +1,14 @@
-Echoes.controller( 'EchoesCtrl', function EchoesCtrl( $scope, $location, filterFilter ){
+Echoes.controller( 'EchoesCtrl', function EchoesCtrl( $scope, $http ){
+	
+	$scope.queryProvider = function() {
+		var url = 'https://gdata.youtube.com/feeds/api/videos?q=' + 
+			$scope.query + '&alt=jsonc&v=2&start-index=' + 
+			1 + 
+			'&max-results=' + 24;
+		$http.get(url).success(function(data){
+			$scope.youtubeResults = data.data.items;
+		});
+	};
 
+	$scope.queryProvider();
 });
