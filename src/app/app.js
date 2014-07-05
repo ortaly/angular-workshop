6b13f900d1a73dde913db21da5de476e568f2088
orizens <farhioren@gmail.com>
2014-07-05 17:38:46 +0300

    adding dat-binding to preset and duration dropdowns

--- src/app/app.js
+++ src/app/app.js
@@ -1,18 +1,32 @@
 function PresetCtrl($scope) {
-	$scope.presetDisplay = 'Preset';
-	$scope.preset1 = 'All';
-	$scope.preset2 = 'Albums';
-	$scope.preset3 = 'Live';
+	$scope.data = {
+		label: 'Preset',
+		items: [
+			'All',
+			'Albums',
+			'Live'
+		]
+	};
+}
+function DurationCtrl($scope){
+	$scope.data = {
+		label: 'Duration',
+		items: [
+			'Any',
+			'Short (less then 4 minutes)',
+			'Medium (4-20 minutes)',
+			'Long (longer than 20 minutes)'
+		]
+	};
 }
 function AppCtrl($scope, $http){
-	// delete return to see service in action
 	return;
 	var url = 'https://www.googleapis.com/youtube/v3/search';
 	var config = {
       params: {
         part: 'snippet,id',
         key: 'AIzaSyB7fFNreY1UzX1la5arnnAi3ZOyvqOV6kk',
-        q: 'alice in chains',
+        q: $scope.query,
         type: 'video',
         maxResults: 50
       }
