cc657af8b52aaf4e721f4d9084e2272779da652f
orizens <farhioren@gmail.com>
2014-08-03 16:26:56 +0300

    router - configure ng-view, html template

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('mediaDeck', []);
+var app = angular.module('mediaDeck', [
+	'ngRoute'
+])
+.config(['$routeProvider', '$locationProvider',
+function($routeProvider, $locationProvider) {
+$routeProvider
+	.when('/', {
+		templateUrl: 'app/partials/youtube.videos.tpl.html'
+		// controller: 'AppCtrl'
+		// controllerAs: 'book'
+	})
+
+	.otherwise({
+		redirectTo: '/'
+	});
+}]);
