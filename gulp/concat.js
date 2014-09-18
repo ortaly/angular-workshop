fbd2d5b52ed687acaa1b428779073d3e73de96a1
orizens <farhioren@gmail.com>
2014-09-18 08:53:10 +0300

    gulp - adding sourcemaps to concat method

--- gulp/concat.js
+++ gulp/concat.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 
 module.exports = gulp.task('build', function() {
   gulp.src(['./src/app/**/*.js', '!./src/app/bundle.js'])
+  	.pipe(sourcemaps.init())
     .pipe(concat('all.js'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./src/app'));
 });
