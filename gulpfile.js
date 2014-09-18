74be95fa9753a9955238fd1d4075fe9336bce8a5
orizens <farhioren@gmail.com>
2014-09-18 08:41:22 +0300

    gulp - create and add jshint task

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var jshint = require('gulp-jshint');
+var gulp   = require('gulp');
+//configure grunt
+var configs = {
+	paths: {
+		files: {
+			js: ['./src/app/**/*.js', '!./src/app/bundle.js']
+		},
+		build: {
+			js: 'bundle.js'
+		}
+	}
+};
+
+gulp.task('lint', function() {
+  return gulp.src(configs.paths.files.js)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
